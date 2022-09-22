c9bffe350333b69acdef77e461e2e09c200139b5
Godwin254 <godwinbee15@gmail.com>
2022-09-22 20:46:08 +0300

    add mongoose library && User schema

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema();
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
