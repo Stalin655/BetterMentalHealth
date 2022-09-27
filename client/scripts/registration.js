d01b3d8f5a22ec49d26d74742459db15ce5509b1
stalin655 <stalinmortice655@gmail.com>
2022-09-27 22:08:23 +0300

    Login form

--- client/scripts/registration.js
+++ client/scripts/registration.js
@@ -1,18 +1,26 @@
-const forms= document.querySelector(".forms"), pwShowHide = document.querySelectorAll(".eye-icon"),
+const forms= document.querySelector(".forms"), 
+pwShowHide = document.querySelectorAll(".eye-icon"),
 links = document.querySelectorAll(".link");
 
 pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".passsword");
+    eye-icon.addEventListener("click", () => {
+        let pwFields = eye-icon.parentElement.parentElement.querySelectorAll(".passsword");
         
         pwFields.forEach(password => {
             if(password.type === "password"){
                 password.type = "text";
-                eyeIcon.classList.replace("bx-hide", "bx-show");
+                eye-icon.classList.replace("bx-hide", "bx-show");
                 return;
             }
             password.type = "password";
-            eyeIcon.classList.replace("bx-show", "bx-hide");
+            eye-icon.classList.replace("bx-show", "bx-hide");
         })
+    })
+})
+
+links.forEach(link => {
+    link.addEventListener("click", e =>{
+        e.preventDefault();
+        forms.classList.toggle("show-signup");
     })
 })
